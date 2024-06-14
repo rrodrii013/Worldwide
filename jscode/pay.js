3ab9e0dcaac5c592bfc6fff39499afec1160d0da
rodriTech <rrodrii013@gmail.com>
2024-06-13 23:45:38 -0300

    AdventureTravel

--- jscode/pay.js
+++ jscode/pay.js
@@ -4,12 +4,16 @@ const card = document.querySelector('.card');
 const fragment = document.createDocumentFragment();
 const options = {headers: {Authorization: `Bearer ${KEYS.secret}`}}
 
-let products, price;
-
+let products, prices;
 Promise.all([
     fetch("https://api.stripe.com/v1/products", options),
     fetch("https://api.stripe.com/v1/prices", options)
 ])
 
 .then(responses => Promise.all(responses.map(res => res.json())))
-.then(json => console.log(json));
+.then(json => {
+    products = json[0].data;
+    prices = json[1].data;
+    console.log(products, prices)
+}
+);

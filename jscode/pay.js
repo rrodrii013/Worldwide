ccd90eb12f9ca22e1bff2b1b342df0143889dcce
rodriTech <rrodrii013@gmail.com>
2024-06-29 22:05:04 -0300

    AdventureTravel

--- jscode/pay.js
+++ jscode/pay.js
@@ -1,75 +1,74 @@
-
 import KEYS from "../jscode/keys.js";
 
-let cards = document.querySelectorAll('.card');
-let moneyValor = num => `${num.slice(0, -2)}`;
+let cards = document.querySelectorAll(".card");
+let moneyValor = (num) => `${num.slice(0, -2)}`;
 const options = { headers: { Authorization: `Bearer ${KEYS.secret}` } };
 
 let products, prices;
 Promise.all([
-    fetch("https://api.stripe.com/v1/products", options),
-    fetch("https://api.stripe.com/v1/prices", options)
+  fetch("https://api.stripe.com/v1/products", options),
+  fetch("https://api.stripe.com/v1/prices", options),
 ])
-.then(responses => Promise.all(responses.map(res => res.json())))
-.then(json => {
+  .then((responses) => Promise.all(responses.map((res) => res.json())))
+  .then((json) => {
     products = json[0].data;
     prices = json[1].data;
 
     prices.forEach((el, index) => {
-        // I connected the arrays
-        let productData = products.filter(product => product.id === el.product);
+      // I connected the arrays
+      let productData = products.filter((product) => product.id === el.product);
 
-        if (productData.length > 0 && index < cards.length) {
+      if (productData.length > 0 && index < cards.length) {
+        //where i'm call the information
+        let nameData = productData[0].name;
+        let imgToAdd = productData[0].images[0];
+        let usd = el.currency.toUpperCase();
+        let priceToAdd = moneyValor(el.unit_amount_decimal);
 
-            //where i'm call the information
-            let nameData = productData[0].name;
-            let imgToAdd = productData[0].images[0];
-            let usd = el.currency.toUpperCase();
-            let priceToAdd = moneyValor(el.unit_amount_decimal); 
+        //where i'll put the information
+        let card = cards[index];
+        card.setAttribute("data-price", el.id);
+        let nameElement = card.querySelector(".country-name");
+        let imgElement = card.querySelector(".country");
+        let priceElement = card.querySelector(".cost");
 
-            //where i'll put the information
-            let card = cards[index];
-            card.setAttribute("data-price", el.id);
-            let nameElement = card.querySelector(".country-name");
-            let imgElement = card.querySelector(".country");
-            let priceElement = card.querySelector(".cost");
+        //take the information and put in the design place
 
-            //take the information and put in the design place
-
-            if(nameElement) {
-                nameElement.innerHTML = nameData.toUpperCase();
-            }
-
-            if(imgElement) {
-                imgElement.src = imgToAdd;
-            }
-
-            if(priceElement){
-                priceElement.innerHTML = `${usd} ${priceToAdd}`
-            }
-
+        if (nameElement) {
+          nameElement.innerHTML = nameData.toUpperCase();
         }
-    })
-})
-.catch(error => console.error('Error fetching data:', error));
 
+        if (imgElement) {
+          imgElement.src = imgToAdd;
+        }
 
-document.addEventListener("click", e => {
+        if (priceElement) {
+          priceElement.innerHTML = `${usd} ${priceToAdd}`;
+        }
+      }
+    });
+  })
+  .catch((error) => console.error("Error fetching data:", error));
 
-    if(e.target.matches(".card *")) {  //* select all the .card´s childs
-        let priceId = e.target.parentElement.getAttribute("data-price");
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".card *")) {
+    //* select all the .card´s childs
+    let priceId = e.target.parentElement.getAttribute("data-price");
 
-        Stripe(KEYS.public).redirectToCheckout({
-            lineItems: [{
-                price: priceId,
-                quantity: 1
-            }],
-            mode: "payment", //Use payment for a single payment. If u want that will be mensual you should use "suscribtion"
-            successUrl:"http://127.0.0.1:5500/assets/cancel.html" , //We find this with window.location.href
-            cancelUrl:"http://127.0.0.1:5500/assets/sucess.html"
-        })
-        .then(err => {
-            err = "Ocurrio un error:" + err;
-        })
-    }
-})
+    Stripe(KEYS.public)
+      .redirectToCheckout({
+        lineItems: [
+          {
+            price: priceId,
+            quantity: 1,
+          },
+        ],
+        mode: "payment", //Use payment for a single payment. If u want that will be mensual you should use "suscribtion"
+        successUrl: "http://127.0.0.1:5500/assets/cancel.html", //We find this with window.location.href
+        cancelUrl: "http://127.0.0.1:5500/assets/sucess.html",
+      })
+      .then((err) => {
+        err = "Ocurrio un error:" + err;
+      });
+  }
+});
--- jscode/script.js
+++ jscode/script.js
@@ -1,247 +1,237 @@
 //A P I
 
 const OpinionUsers = async () => {
-    try {
-        const respuesta = await fetch(`https://randomuser.me/api/?results=8`);
+  try {
+    const respuesta = await fetch(`https://randomuser.me/api/?results=8`);
 
-        if (respuesta.status === 200) {
-            const datos = await respuesta.json();
-            const opiniones = datos.results;
+    if (respuesta.status === 200) {
+      const datos = await respuesta.json();
+      const opiniones = datos.results;
 
-            //Recorre cada tarjeta
-            document.querySelectorAll('.opinion_card').forEach((tarjeta, index) => {
+      //Recorre cada tarjeta
+      document.querySelectorAll(".opinion_card").forEach((tarjeta, index) => {
+        //Obtiene los datos de la opinion correspondiente
+        const opinion = opiniones[index];
 
-                //Obtiene los datos de la opinion correspondiente
-                const opinion = opiniones[index];
+        const img = tarjeta.querySelector(".pics_profile");
+        const userName = tarjeta.querySelector(".user_name");
 
-                const img = tarjeta.querySelector('.pics_profile');
-                const userName = tarjeta.querySelector('.user_name');
+        img.src = opinion.picture.large;
 
-                img.src = opinion.picture.large;
-
-                //Coloca cada nombre en la variable userName
-                userName.textContent = `${opinion.name.first} ${opinion.name.last}`
-            
-            })
-        } else if (respuesta.status === 401) {
-            console.log('401 Unauthorized');
-        } else if (respuesta.status === 404) {
-            console.log('404 Not Found');
-        }
-    } catch (error) {
-        console.log('Error:', error);
+        //Coloca cada nombre en la variable userName
+        userName.textContent = `${opinion.name.first} ${opinion.name.last}`;
+      });
+    } else if (respuesta.status === 401) {
+      console.log("401 Unauthorized");
+    } else if (respuesta.status === 404) {
+      console.log("404 Not Found");
     }
+  } catch (error) {
+    console.log("Error:", error);
+  }
 };
 
 // Llamando la función al cargar la página
-document.addEventListener('DOMContentLoaded', OpinionUsers);
+document.addEventListener("DOMContentLoaded", OpinionUsers);
 
 // RESPONSIVE MENU
 
-let menuBtn = document.querySelector('.icon-menu');
-let cancelIcon = document.querySelector('.icon-cancel-circle');
-let nav = document.querySelector('.responsive-nav');
-nav.style.display = 'none';
-menuBtn.addEventListener('click', () => {
-    nav.style.display = 'flex';
+let menuBtn = document.querySelector(".icon-menu");
+let cancelIcon = document.querySelector(".icon-cancel-circle");
+let nav = document.querySelector(".responsive-nav");
+nav.style.display = "none";
+menuBtn.addEventListener("click", () => {
+  nav.style.display = "flex";
 });
-cancelIcon.addEventListener('click', () => {
-    nav.style.display = 'none'
+cancelIcon.addEventListener("click", () => {
+  nav.style.display = "none";
 });
-
 
 //SCROLL
 
-let btnHome = document.querySelector('.btn-home');
+let btnHome = document.querySelector(".btn-home");
 
 function scrollWindow() {
-    let contact = document.querySelector('.contactt');
-    contact.scrollIntoView({behavior: 'smooth'});
+  let contact = document.querySelector(".contactt");
+  contact.scrollIntoView({ behavior: "smooth" });
 }
 
-btnHome.addEventListener('click', () => {
-    scrollWindow();
-})
+btnHome.addEventListener("click", () => {
+  scrollWindow();
+});
 
-
-//CRONOMETRO 
+//CRONOMETRO
 let cronometro;
 let crono = document.getElementById("crono");
 
-
 function cronoRandom() {
-   return Math.floor(Math.random() * 60)
+  return Math.floor(Math.random() * 60);
 }
 
-const numeroAleatorio = cronoRandom(); 
+const numeroAleatorio = cronoRandom();
 
 function segundosRandom() {
-    return Math.floor(Math.random() * 60)
+  return Math.floor(Math.random() * 60);
 }
- 
-const segundosAleatorio = segundosRandom(); 
+
+const segundosAleatorio = segundosRandom();
 
 let horas = numeroAleatorio;
 let minutos = Math.trunc(numeroAleatorio / 2);
-let segundos = segundosAleatorio ;
-
+let segundos = segundosAleatorio;
 
 function actualizaContador() {
+  segundos--;
 
+  if (segundos === 0) {
+    segundos = 59;
+    minutos--;
+  }
 
-        segundos-- ;
+  if (minutos === 0) {
+    minutos = 59;
+    horas--;
+  }
 
-        if (segundos === 0 ){
-            segundos = 59 ;
-            minutos --;
-        }
+  if (horas === 0 && minutos === 0 && segundos === 0) {
+    clearInterval(cronometro);
+    return;
+  }
 
-        if ( minutos === 0 ){
-            minutos = 59 ;
-            horas--
-        }
-
-        if(horas === 0  && minutos === 0 && segundos === 0) {
-            clearInterval(cronometro)
-            return 
-        }
-
-     crono.innerHTML = formatNum(horas) + ':' + formatNum(minutos) + ':' + formatNum(segundos);
-
+  crono.innerHTML =
+    formatNum(horas) + ":" + formatNum(minutos) + ":" + formatNum(segundos);
 }
 
 function formatNum(num) {
-        return num < 10 ? '0' + num : num
+  return num < 10 ? "0" + num : num;
 }
-  
-crono.innerHTML = formatNum(horas) + ':' + formatNum(minutos) + ':' + formatNum(segundos);
 
+crono.innerHTML =
+  formatNum(horas) + ":" + formatNum(minutos) + ":" + formatNum(segundos);
 
 function cronometroOn() {
-        cronometro = setInterval(actualizaContador, 1000)
+  cronometro = setInterval(actualizaContador, 1000);
 }
-  cronometroOn()
+cronometroOn();
 
+//CARRUSEL OPINIONES
 
-  //CARRUSEL OPINIONES 
+document.addEventListener("DOMContentLoaded", function () {
+  let btnNext = document.getElementById("btn_next");
+  let opiniones = document.querySelectorAll(".opinion_card");
+  let opinionesVisibles = 4; // Queremos mostrar solo 4 opiniones por vez
+  let indiceActual = 0; // Variable para llevar el seguimiento del índice actual del carrusel
 
+  // Ocultar todas las opiniones excepto las primeras opinionesVisibles
+  for (let i = opinionesVisibles; i < opiniones.length; i++) {
+    opiniones[i].style.display = "none";
+  }
 
-document.addEventListener('DOMContentLoaded', function() {
-    let btnNext = document.getElementById("btn_next");
-    let opiniones = document.querySelectorAll(".opinion_card");
-    let opinionesVisibles = 4; // Queremos mostrar solo 4 opiniones por vez
-    let indiceActual = 0; // Variable para llevar el seguimiento del índice actual del carrusel
-
-// Ocultar todas las opiniones excepto las primeras opinionesVisibles
-    for (let i = opinionesVisibles; i < opiniones.length; i++) {
-        opiniones[i].style.display = 'none';
-    }
-
-    btnNext.addEventListener("click", function() {
+  btnNext.addEventListener("click", function () {
     // Ocultar la primera opinión actual
-        opiniones[indiceActual].style.display = 'none';
+    opiniones[indiceActual].style.display = "none";
 
     // Incrementar el índice actual
-       indiceActual++;
+    indiceActual++;
 
     // Si el índice actual supera el número de opiniones, lo reinicia a 0
-        if (indiceActual >= opiniones.length) {
-            indiceActual = 0;
-        }
+    if (indiceActual >= opiniones.length) {
+      indiceActual = 0;
+    }
 
     // Mostrar la próxima opinión en el carrusel
-        opiniones[(indiceActual + opinionesVisibles - 1) % opiniones.length].style.display = 'flex'; // Usando % aseguramos que el índice calculado esté dentro del rango válido
-    });
+    opiniones[
+      (indiceActual + opinionesVisibles - 1) % opiniones.length
+    ].style.display = "flex"; // Usando % aseguramos que el índice calculado esté dentro del rango válido
+  });
 });
-
-
 
 //CARRUSEL VIAJES
 
 let index = 0;
-const viajes = document.querySelectorAll('.card');
+const viajes = document.querySelectorAll(".card");
 const totalViajes = viajes.length;
 let responsiveWindow = window.innerWidth;
 let carrusel;
 let carrusel767;
 
 // If window < 767 carruselMil star when the page load.
-window.addEventListener('load', (event) => {
-    carruselMil();
-})
+window.addEventListener("load", (event) => {
+  carruselMil();
+});
 
 function carruselTop() {
-    document.querySelector('.active').classList.remove('active');
-    
-    index = (index + 1) % totalViajes; // Vuelve a 0 después de la última tarjeta
-    viajes[index].classList.add('active');
+  document.querySelector(".active").classList.remove("active");
 
-    const offset = -index * 470; // 450px width + 20px margin
-    document.getElementById('carrusel').style.transform = `translateX(${offset}px)`;
+  index = (index + 1) % totalViajes; // Vuelve a 0 después de la última tarjeta
+  viajes[index].classList.add("active");
+
+  const offset = -index * 470; // 450px width + 20px margin
+  document.getElementById(
+    "carrusel"
+  ).style.transform = `translateX(${offset}px)`;
 }
 
 function startCarrusel() {
-    carrusel = setInterval(carruselTop, 2000);
+  carrusel = setInterval(carruselTop, 2000);
 }
 
 function stopCarrusel() {
-    clearInterval(carrusel);
+  clearInterval(carrusel);
 }
 
 function resetCarruselPosition() {
-    document.getElementById('carrusel').style.transform = 'translateX(0)';
+  document.getElementById("carrusel").style.transform = "translateX(0)";
 }
 
 function maxMil() {
-    stopCarrusel();
-     if(responsiveWindow < 1000 ){
-        document.querySelector('.active').classList.remove('active');
-    
-        index = (index + 1) % totalViajes; // Vuelve a 0 después de la última tarjeta
-        viajes[index].classList.add('active');
+  stopCarrusel();
+  if (responsiveWindow < 1000) {
+    document.querySelector(".active").classList.remove("active");
 
-         offset = -index * 470; // 500px width + 20px margin
-        document.getElementById('carrusel').style.transform = `translateX(${offset}px)`
-       
-    }
+    index = (index + 1) % totalViajes; // Vuelve a 0 después de la última tarjeta
+    viajes[index].classList.add("active");
 
+    offset = -index * 470; // 500px width + 20px margin
+    document.getElementById(
+      "carrusel"
+    ).style.transform = `translateX(${offset}px)`;
+  }
 }
 
 function carruselMil() {
-    carrusel767 = setInterval(maxMil, 2000)
+  carrusel767 = setInterval(maxMil, 2000);
 }
 
-function stopMil(){
-    clearInterval(carrusel767)
+function stopMil() {
+  clearInterval(carrusel767);
 }
 
 function handleResize() {
-    responsiveWindow = window.innerWidth < 479 ;
-    if (responsiveWindow) {
-        startCarrusel();
-    } else {
-        stopMil();
-        stopCarrusel();
-        resetCarruselPosition();
-    }
-}
-    
-    function handleResize1() {
-        responsiveWindow = window.innerWidth < 767 ;
-        if (responsiveWindow) {
-            carruselMil();
-        } else {
-            stopMil();
-            stopCarrusel();
-            resetCarruselPosition();
-        }
-        
-    }
-
-window.addEventListener('resize', function(){
+  responsiveWindow = window.innerWidth < 479;
+  if (responsiveWindow) {
+    startCarrusel();
+  } else {
     stopMil();
     stopCarrusel();
-    handleResize();
-    handleResize1();
-});
+    resetCarruselPosition();
+  }
+}
 
+function handleResize1() {
+  responsiveWindow = window.innerWidth < 767;
+  if (responsiveWindow) {
+    carruselMil();
+  } else {
+    stopMil();
+    stopCarrusel();
+    resetCarruselPosition();
+  }
+}
+
+window.addEventListener("resize", function () {
+  stopMil();
+  stopCarrusel();
+  handleResize();
+  handleResize1();
+});

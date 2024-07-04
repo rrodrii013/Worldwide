168d5b8ba9a902a227cf6a57735f5514880436a7
rodriTech <rrodrii013@gmail.com>
2024-07-04 17:09:47 -0300

    AdventureTravel

--- jscode/pay.js
+++ jscode/pay.js
@@ -1,5 +1,8 @@
 import KEYS from "../jscode/keys.js";
 
+let offerCard1 = document.querySelector(".tarjeta1");
+let offerCard2 = document.querySelector(".tarjeta2");
+let offerCard3 = document.querySelector(".tarjeta3");
 let cards = document.querySelectorAll(".card");
 let moneyValor = (num) => `${num.slice(0, -2)}`;
 const options = { headers: { Authorization: `Bearer ${KEYS.secret}` } };
@@ -11,12 +14,43 @@ Promise.all([
 ])
   .then((responses) => Promise.all(responses.map((res) => res.json())))
   .then((json) => {
+
+    console.log(json);
     products = json[0].data;
     prices = json[1].data;
 
-    prices.forEach((el, index) => {
+
+
+    let finallyProducts = products.slice();
+    let finallyPrices = prices.slice();
+
+    finallyProducts.splice(0, 2);
+    finallyPrices.splice(0, 2);
+ 
+
+    console.log(finallyProducts);
+    console.log(finallyPrices);
+
+             //T E S T S
+    //packs's names
+    /*let grandeItalia = products[0].name //Grande Italia
+    console.log(grandeItalia)
+    let grandItaliaPrice = prices[1].currency + ' ' + prices[1].unit_amount_decimal //Grande Italia price
+    console.log(grandItaliaPrice)
+
+   
+
+    let outProducts = products.shift();
+    let outPrices = prices.shift();
+
+    console.log(products)
+    console.log(prices) */
+
+
+
+    finallyPrices.forEach((el, index) => {
       // I connected the arrays
-      let productData = products.filter((product) => product.id === el.product);
+      let productData = finallyProducts.filter((product) => product.id === el.product);
 
       if (productData.length > 0 && index < cards.length) {
         //here i'm call the information
@@ -55,6 +89,8 @@ document.addEventListener("click", (e) => {
     //* select all the .card´s childs
     let priceId = e.target.parentElement.getAttribute("data-price");
 
+    console.log(priceId);
+
     Stripe(KEYS.public)
       .redirectToCheckout({
         lineItems: [
@@ -64,8 +100,8 @@ document.addEventListener("click", (e) => {
           },
         ],
         mode: "payment", //Use payment for a single payment. If u want that will be mensual you should use "suscribtion"
-        successUrl: "http://127.0.0.1:5500/assets/cancel.html", //We find this with window.location.href
-        cancelUrl: "http://127.0.0.1:5500/assets/sucess.html",
+        successUrl: "http://127.0.0.1:5500/assets/sucess.html",  //We find this with window.location.href
+        cancelUrl: "http://127.0.0.1:5500/assets/cancel.html",
       })
       .then((err) => {
         err = "Ocurrio un error:" + err;

1891dacf6a837addd0283c45214a89a9a987f1c5
rodriTech <rrodrii013@gmail.com>
2024-07-16 22:36:32 -0300

    AdventureTravel

--- jscode/mail.js
+++ jscode/mail.js
@@ -1,2 +1,15 @@
 const btn = document.querySelector(".btn");
-console.log(btn.length);
+const emailSent = document.querySelector(".email-sent");
+const contacDiv = document.getElementById("contact");
+const aceptBack = document.querySelector(".email-sent-ok");
+const contactArticle = document.querySelector(".contactt");
+
+btn.addEventListener("click", () => {
+    emailSent.style.display = "flex";
+    contacDiv.style.display = "none";
+})
+
+aceptBack.addEventListener("click", () => {
+    emailSent.style.display = "none";
+    contacDiv.style.display = "flex";
+})

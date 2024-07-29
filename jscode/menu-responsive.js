f217605bcf35d1ed4f160c618ba99075a3b8e7c5
rodriTech <rrodrii013@gmail.com>
2024-07-29 09:44:12 -0300

    removing responsive nav with listener

--- jscode/menu-responsive.js
+++ jscode/menu-responsive.js
@@ -4,9 +4,17 @@ let menuBtn = document.querySelector(".icon-menu");
 let cancelIcon = document.querySelector(".icon-cancel-circle");
 let nav = document.querySelector(".responsive-nav");
 nav.style.display = "none";
+
 menuBtn.addEventListener("click", () => {
   nav.style.display = "flex";
 });
+
 cancelIcon.addEventListener("click", () => {
   nav.style.display = "none";
 });
+
+document.addEventListener("click", function(event){
+if(!nav.contains(event.target) && !menuBtn.contains(event.target) && !cancelIcon.contains(event.target)) {
+  nav.style.display = "none";
+} 
+})
